18d86542bb9e246b3df02f8f49228bc38d090fea
bruck-dev <141885443+bruck-dev@users.noreply.github.com>
2025-02-28 22:07:02 -0600

    Add license to padlocks
    1) Forgot to add my Creative Commons license to the plugin, whoops.

--- padlocks/sh_plugin.lua
+++ padlocks/sh_plugin.lua
@@ -4,6 +4,12 @@ local PLUGIN = PLUGIN
 PLUGIN.name = "Padlocks"
 PLUGIN.description = "Adds a system of placeable and breakable padlocks."
 PLUGIN.author = "bruck"
+PLUGIN.license = [[
+Copyright 2025 bruck
+This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+]]
+
 
 -- list of weapon classes that cannot be used to break padlocks
 PLUGIN.PadlockWeaponsBlacklist = {
