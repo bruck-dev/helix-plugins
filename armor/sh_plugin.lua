c71ed542180b936d2c2b159ce16b9bcf3b7542cd
bruck-dev <141885443+bruck-dev@users.noreply.github.com>
2025-05-17 14:16:23 -0500

    Add license to armor plugin
    1) Forgot to add my normal license, since this one was original.

--- armor/sh_plugin.lua
+++ armor/sh_plugin.lua
@@ -4,6 +4,11 @@ local PLUGIN = PLUGIN
 PLUGIN.name = "Armor Outfit System"
 PLUGIN.description = "Adds support for a hitgroup-based armor system, complete with resistances and durability."
 PLUGIN.author = "bruck"
+PLUGIN.license = [[
+Copyright 2025 bruck
+This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+]]
 
 ix.util.IncludeDir(PLUGIN.folder .. "/meta", true)
 ix.util.IncludeDir(PLUGIN.folder .. "/hooks", true)
