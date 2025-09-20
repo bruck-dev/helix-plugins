fad22ea7e1830e69a44d8c8c16d2d959e691e646
bruck-dev <141885443+bruck-dev@users.noreply.github.com>
2025-09-19 21:00:14 -0500

    INVPAC: Add license
    1) Forgot to add the release license lol

--- inventorypac/sh_plugin.lua
+++ inventorypac/sh_plugin.lua
@@ -1,6 +1,11 @@
 PLUGIN.name = "Inventory PAC Items"
 PLUGIN.author = "bruck"
 PLUGIN.description = "Adds support for PAC data on items based on inventory presence, in addition to equip status."
+PLUGIN.license = [[
+Copyright 2025 bruck
+This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+]]
 
 if (!pace) then return end
 
