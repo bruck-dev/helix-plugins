b4e0abe063036a4ce7a07c764378a74cc22abcd8
bruck-dev <141885443+bruck-dev@users.noreply.github.com>
2025-09-25 22:45:15 -0500

    AMBIENT: Adjust stop/start logic
    1) Fixed the ShouldBlockAmbientMusic() hook actually firing on the REVERSE condition - it should have checked if it returned true, lol.
    2) Moved ShouldBlockAmbientMusic() into it's own conditional that adjusts the timer repeat time to be JUST the new min + max random, as to not include the length of the previous track (if it existed).
    3) AmbientMusicStop() now accepts a keepTimer argument - if true, the music will be stopped but the timer will keep running and keep servicing the next track as expected. This means music can be stopped temporarily without needing to restart it fully every time.

--- ambientmusic/meta/sh_player.lua
+++ ambientmusic/meta/sh_player.lua
@@ -3,6 +3,7 @@ local PLUGIN = PLUGIN
 local PLAYER = FindMetaTable("Player")
 
 if CLIENT then
+    -- starts the ambient music and associated timer. once ran, will keep going until stopped by an external source
     function PLAYER:AmbientMusicStart()
         if !ix.config.Get("allowAmbientMusic", true) or !ix.option.Get("ambientMusicEnable", true) then return end  -- disable
         if timer.Exists("ixAmbientMusic") then return end                                                           -- already on
@@ -13,39 +14,45 @@ if CLIENT then
         table.Shuffle(tracks)
 
         timer.Create("ixAmbientMusic", math.random(ix.option.Get("ambientMusicIntMin", 120), ix.option.Get("ambientMusicIntMax", 300)), 0, function()
-            if timer.Exists("ixAmbientMusic") and hook.Run("ShouldBlockAmbientMusic", LocalPlayer()) != false then
-                index = index + 1
-                if index > #tracks then
-                    index = 1
-                    table.Shuffle(tracks)
+            if timer.Exists("ixAmbientMusic") then
+                if hook.Run("ShouldBlockAmbientMusic", LocalPlayer()) != true then
+                    index = index + 1
+                    if index > #tracks then
+                        index = 1
+                        table.Shuffle(tracks)
+                    end
+
+                    local track = tracks[index]
+
+                    sound.PlayFile("sound/" .. track, "noplay", function(channel, err)
+                        if !IsValid(channel) then return end
+                        LocalPlayer().ambientMusicChannel = channel
+                        channel:SetVolume(ix.option.Get("ambientMusicVolume", 100) / 100)
+                        channel:Play()
+                    end)
+
+                    timer.Adjust("ixAmbientMusic", SoundDuration(track) + math.random(ix.option.Get("ambientMusicIntMin", 120), ix.option.Get("ambientMusicIntMax", 300)), nil, nil)
+                else -- if unable to play a new track, skip this one and set the timer to check later. technically not required but shortens the retry time by not including the last track length
+                    timer.Adjust("ixAmbientMusic", math.random(ix.option.Get("ambientMusicIntMin", 120), ix.option.Get("ambientMusicIntMax", 300)), nil, nil)
                 end
-
-                local track = tracks[index]
-
-                sound.PlayFile("sound/" .. track, "noplay", function(channel, err)
-                    if !IsValid(channel) then return end
-                    LocalPlayer().ambientMusicChannel = channel
-                    channel:SetVolume(ix.option.Get("ambientMusicVolume", 100) / 100)
-                    channel:Play()
-                end)
-
-                timer.Adjust("ixAmbientMusic", SoundDuration(track) + math.random(ix.option.Get("ambientMusicIntMin", 120), ix.option.Get("ambientMusicIntMax", 300)), nil, nil)
             end
         end)
 
     end
 
-    function PLAYER:AmbientMusicStop()
+    -- stops the currently playing ambient track and removes the timer until re-enabled (unless keepTimer is true)
+    function PLAYER:AmbientMusicStop(keepTimer)
         if self.ambientMusicChannel then
             self.ambientMusicChannel:Stop()
             self.ambientMusicChannel = nil
         end
 
-        if timer.Exists("ixAmbientMusic") then
+        if timer.Exists("ixAmbientMusic") and !keepTimer then
             timer.Remove("ixAmbientMusic")
         end
     end
 
+    -- adjusts the volume of the currently playing ambient track based on the specified value, as a percent
     function PLAYER:AmbientMusicVolume(vol)
         if self.ambientMusicChannel then
             self.ambientMusicChannel:SetVolume(vol / 100)
