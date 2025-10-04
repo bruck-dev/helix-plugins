6d8e20c94389347324a4a12a2d9935be7fa57ca3
bruck-dev <141885443+bruck-dev@users.noreply.github.com>
2025-10-04 16:16:55 -0500

    CLOTHING: Fix GetWornItems() base check
    1) In cases where an item didn't have a base, GetWornItems() would throw an error. This fixes that issue.

--- bonemerged_clothing/meta/sh_character.lua
+++ bonemerged_clothing/meta/sh_character.lua
@@ -5,7 +5,7 @@ function CHAR:GetWornItems()
     local worn = {}
 
     for k, _ in self:GetInventory():Iter() do
-        if string.find(k.base, "wearable") and k:GetData("equip", false) then
+        if (k.base and string.find(k.base, "wearable")) and k:GetData("equip", false) then
             table.insert(worn, k)
         end
     end
