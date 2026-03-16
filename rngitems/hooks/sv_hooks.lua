f35c2078e05f0cd593b9af1d7ca50eb2c917f11c
bruck-dev <141885443+bruck-dev@users.noreply.github.com>
2026-03-16 16:55:03 -0500

    RNGITEMS: Rework model set process
    1) Reworked the model set process to apply the model without the need for a custom hook, and in a way that works before a player picks it up

--- rngitems/hooks/sv_hooks.lua
+++ rngitems/hooks/sv_hooks.lua
@@ -1,11 +1,19 @@
 
 local PLUGIN = PLUGIN
 
--- the plugin has to be loaded before this fires, so put it VERY FIRST IN YOUR PLUGIN LOAD ORDER, BEFORE ANY ITEMS ARE CREATED
-function PLUGIN:OnItemInstanced(item)
-    if item.rngModels then
-        if item:GetData("model", nil) == nil then
+function PLUGIN:InventoryItemAdded(oldInv, newInv, item)
+    if item.rngModels and item:GetData("model", nil) == nil then
+        item:SetData("model", item.rngModels[math.random(1, #item.rngModels)])
+    end
+end
+
+function PLUGIN:OnItemSpawned(entity)
+    local itemTable = entity:GetItemTable()
+    if itemTable.rngModels then
+        local item = entity.ixItemID and ix.item.instances[entity.ixItemID]
+        if item and item:GetData("model", nil) == nil then
             item:SetData("model", item.rngModels[math.random(1, #item.rngModels)])
+            entity:SetModel(item:GetModel())
         end
     end
 end
